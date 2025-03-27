cfb1259a88b62698f4670880a9e8706931e8da26
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-03-27 19:52:16 +0100

    style: animate footer hover effect

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,15 @@
-<footer class="border-t border-muted!">
+<footer class="border-muted! border-t">
 	<div class="mx-auto max-w-(--breakpoint-xl) px-4 py-8">
 		<div class="flex flex-col items-center justify-between gap-4 md:flex-row">
 			<div class="flex items-center space-x-1">
 				<span class="text-muted-foreground text-sm">© {new Date().getFullYear()}</span>
-				<span class="text-muted-foreground text-sm"><a href="https://paulplay.studio" target="_blank">PaulPlay</a></span
+				<span class="text-muted-foreground hover:text-primary text-sm transition"
+					><a href="https://paulplay.studio" target="_blank">PaulPlay</a></span
 				>
 			</div>
-			<nav class="flex gap-6 nowrap flex-wrap">
-				<a href="/terms" class="text-muted-foreground hover:text-primary text-sm"> Terms of Use </a>
-				<a href="/privacy" class="text-muted-foreground hover:text-primary text-sm"> Privacy Policy </a>
+			<nav class="nowrap flex flex-wrap gap-6">
+				<a href="/terms" class="text-muted-foreground hover:text-primary text-sm transition"> Terms of Use </a>
+				<a href="/privacy" class="text-muted-foreground hover:text-primary text-sm transition"> Privacy Policy </a>
 				<a
 					href="https://paulplay.studio/imprint"
 					target="_blank"
